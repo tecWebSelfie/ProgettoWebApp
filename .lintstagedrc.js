6b63c9d312c57fc3d4abe00bce713eb0f50a27f7
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-09-19 15:53:06 +0200

    lint-staged installed and configured

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `npm run lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
 module.exports = {
-  "*": "prettier --check --ignore-unknown",
-  "*.{t|j}s?(x)": [buildEslintCommand],
+  "*": "prettier --write --ignore-unknown",
+  "*.t|js?(x)": [buildEslintCommand],
 };
 
 // module.exports = {
