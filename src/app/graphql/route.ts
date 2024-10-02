bc83d775819e25ca281d08cf99f0762d6ddaf41b
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-03 00:36:09 +0200

    now graphql api connects to 'db' database if db name is not specified

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -8,7 +8,7 @@ const username = process.env.MONGO_INITDB_ROOT_USERNAME || "";
 const password = process.env.MONGO_INITDB_ROOT_PASSWORD || "";
 const hostname = process.env.DB_HOSTNAME || "127.0.0.1";
 const port = process.env.DB_PORT || 27017;
-const dbname = process.env.DB_NAME || "selfie";
+const dbname = process.env.DB_NAME || "db";
 const dbauthsrc = process.env.DB_AUTHSRC || null;
 
 mongoose.connect(
