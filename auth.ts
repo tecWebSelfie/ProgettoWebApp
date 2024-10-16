dc8560cead243169bde0cbc618acf6e0b68414b7
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-17 00:15:12 +0200

    next-auth config file

---
+++ auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        return {};
+      },
+    }),
+  ],
+  basePath: "/auth",
+});
