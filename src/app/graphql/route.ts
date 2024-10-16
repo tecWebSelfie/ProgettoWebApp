6c8fd301e231f37795bb3d801ea2542d7f386130
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-17 00:22:36 +0200

    genericAuth set to granular mode

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server";
 import { createYoga } from "graphql-yoga";
 import { graphqlschema } from "../../db/models/event";
 import mongoose from "mongoose";
+import { ResolveUserFn, useGenericAuth } from "@envelop/generic-auth";
 
 const protocol = process.env.DB_PROTOCOL || "mongodb";
 const username = process.env.MONGO_INITDB_ROOT_USERNAME || "";
@@ -11,12 +12,26 @@ const port = process.env.DB_PORT || 27017;
 const dbname = process.env.DB_NAME || "db";
 const dbauthsrc = process.env.DB_AUTHSRC || null;
 
+//this is the function that will be passed to genericAuth. It must return either the user object or null
+const resolveUserFn: ResolveUserFn<{}> = async function (ctx) {
+  return { name: "pippo" };
+};
+
 mongoose.connect(
   `${protocol}://${username}:${password}@${hostname}:${port}/${dbname}`,
   { authSource: dbauthsrc },
 );
 
-const app = createYoga({ schema: graphqlschema });
+const app = createYoga({
+  schema: graphqlschema,
+  plugins: [
+    // eslint-disable-next-line
+    useGenericAuth({
+      mode: "protect-granular",
+      resolveUserFn: resolveUserFn,
+    }),
+  ],
+});
 
 export async function GET(request: NextRequest) {
   return app.handle(request);
