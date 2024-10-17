300aca347d0900362b0358dd2363e5463f00470b
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-17 14:45:02 +0200

    added debug opt-in

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: process.env.NODE_ENV == "development",
   providers: [
     Credentials({
       credentials: {
