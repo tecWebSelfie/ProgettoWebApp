7ecd4e6b0ea387a6a8a167458c62586348b1c9ae
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-17 17:26:42 +0200

    removed self interface definition since it already exists

--- src/db/models/calendar.ts
+++ src/db/models/calendar.ts
@@ -1,22 +1,14 @@
-import { ICalCalendarMethod, ICalEventJSONData } from "ical-generator";
+import {
+  ICalCalendarMethod,
+  ICalEventJSONData,
+  ICalCalendarJSONData,
+} from "ical-generator";
 import { calendarModelName } from "./mongo_contract";
 import mongoose, { Schema } from "mongoose";
 
-interface ICalCalendarJSONData {
-  prodId: string;
-  method: ICalCalendarMethod | null;
-  name: string | null;
-  description: string | null;
-  timezone: string | null;
-  source: string | null;
-  url: string | null;
-  scale: string | null;
-  ttl: number | null;
-  events: ICalEventJSONData[];
-  x: { key: string; value: string }[];
-}
-
-const calendarSchema = new Schema<ICalCalendarJSONData>({});
+const calendarSchema = new Schema<ICalCalendarJSONData>({
+  events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+});
 
 const calendarModel = mongoose.model<ICalCalendarJSONData>(
   calendarModelName,
