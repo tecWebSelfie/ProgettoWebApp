41f7bc3966ce169d0e07cda358ae5f80af6f3504
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-31 18:40:57 +0100

    fixed server files

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const createServer = require("http").createServer;
 const parse = require("url").parse;
 const path = require("path");
 const next = require("next");
-const mongoose = require("./db/dbconfig");
 
 process.chdir(path.join(__dirname, "../"));
 
