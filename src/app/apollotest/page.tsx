92053ad65d2430697c7f1a05e2f821c38fcd9b11
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-11-08 18:28:03 +0100

    added example use of apollo that leverages codegen

--- src/app/apollotest/page.tsx
+++ src/app/apollotest/page.tsx
@@ -2,25 +2,22 @@
 
 import { graphql } from "@/gql/gql";
 import { useQuery, gql } from "@apollo/client";
+import Sumier from "./Sumier";
 
 const TEST_QUERY = graphql(`
-  query TestQuery($id: MongoID!) {
-    notification_findById(_id: $id) {
-      title
-      _id
-    }
+  query TestQuery {
+    ...notificationQuery
   }
 `);
 
 export default function ApolloTest() {
-  const id = "6728f4cab054e1d356685c46";
-  const { data, loading, error } = useQuery(TEST_QUERY, { variables: { id } });
+  const { data, loading, error } = useQuery(TEST_QUERY);
 
   return (
     <div>
       {loading && <h1>sto caricando</h1>}
       {error && <h1>{error.message}</h1>}
-      {data && <h1>{data.notification_findById?.title}</h1>}
+      {data && <Sumier notification={data} />}
     </div>
   );
 }
