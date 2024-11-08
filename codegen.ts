7de871d8058513f353911c6508b530ed03278bc7
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-11-08 20:22:30 +0100

    added typescrypt-react-apollo plugin

--- codegen.ts
+++ codegen.ts
@@ -5,11 +5,12 @@ const config: CodegenConfig = {
   schema: "http://localhost:3000/graphql",
   documents: "./src/**/*.tsx",
   ignoreNoDocuments: true,
+
   verbose: true,
   generates: {
     "src/gql/": {
       preset: "client",
-      plugins: [],
+      plugins: ["typescript-react-apollo"],
     },
     "./graphql.schema.json": {
       plugins: ["introspection"],
