3324a62d74b4487db4a72f1232dd2f0ecb637cf9
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-05 15:29:03 +0100

    added sse related configuration

--- src/app/graphql/stream/sseServer.ts
+++ src/app/graphql/stream/sseServer.ts
@@ -1,4 +1,4 @@
+import { createHandler } from "graphql-sse";
 import { schema } from "@/db/gqlschema";
-import { createHandler } from "graphql-sse/lib/use/http";
 
 export const sseServer = createHandler({ schema });
