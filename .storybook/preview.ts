e266c2d079676376e1eeb01d80affdc6aed5c0c6
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-09 14:06:16 +0100

    added tailwind file import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,7 @@
 import type { Preview } from "@storybook/react";
 
 import { withThemeByClassName } from "@storybook/addon-themes";
+import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
