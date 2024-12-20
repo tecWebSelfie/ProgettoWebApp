8c6a063622f85ccfc00a675e75d59da52f0ee6f5
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-20 16:13:28 +0100

    reverted codegen to work locally with schema-graphql

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:3000/graphql",
+  schema: "schema.graphql",
   documents: "./src/**/*.tsx",
   ignoreNoDocuments: true,
   verbose: true,
