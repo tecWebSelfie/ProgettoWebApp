74a63f1df7b7549dfbc14e5761d119d3e887dc38
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-22 23:07:25 +0100

    developing time machine, work in progress

--- src/components/TimeMachine.tsx
+++ src/components/TimeMachine.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { ApolloClient, useApolloClient, useFragment } from "@apollo/client";
 import { Button } from "./ui/button";
 import { Calendar } from "./ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { FaRegHourglassHalf } from "react-icons/fa6";
+import { TIME_MACHINE_FRAGMENT } from "@/localGql/localOperations";
 
 export function TimeMachine() {
+  const apolloClient = useTimeMachine();
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -17,4 +22,22 @@ export function TimeMachine() {
       </PopoverContent>
     </Popover>
   );
+}
+
+function useTimeMachine() {
+  const client = useApolloClient();
+
+  const ref = setTimeMachine(new Date(), client);
+
+  return useFragment({ fragment: TIME_MACHINE_FRAGMENT, from: "ROOT_QUERY" });
+}
+
+function setTimeMachine(NewDate: Date, client: ApolloClient<object>) {
+  return client.writeFragment({
+    id: "ROOT_QUERY",
+    fragment: TIME_MACHINE_FRAGMENT,
+    data: {
+      TimeMachine: NewDate,
+    },
+  });
 }
