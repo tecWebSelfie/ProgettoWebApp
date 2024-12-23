47fd2fc55a5c7769adf2c2e2048748a4d8074a2e
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-23 02:32:02 +0100

    prototype of journal section with textarea and list of journals

--- src/app/graphql/mocks.ts
+++ src/app/graphql/mocks.ts
@@ -3,6 +3,17 @@ import { faker } from "@faker-js/faker";
 //This is mocked data for the graphql server
 export const mocks = {
   MongoID: () => faker.database.mongodbObjectId(),
+  User: () => ({
+    name: faker.person.firstName(),
+    surname: faker.person.lastName(),
+    nickname: faker.animal.bird(),
+  }),
+  Journal: () => ({
+    summary: "journal summary",
+    description: "journal description",
+    start_date: faker.date.recent().toISOString(),
+    last_modified: faker.date.recent().toISOString(),
+  }),
 };
 
 //These are mocked resolvers for the graphql server
--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,3 +1,32 @@
-export default function Note() {
-  return <div></div>;
+"use client";
+
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { JournalsList } from "@/src/components/JournalsList";
+import { graphql, useFragment } from "@/src/gql";
+import { useQuery } from "@apollo/client";
+
+const journalPageQuery = graphql(`
+  query journalPageQuery {
+    ...journalsListFragment
+  }
+`);
+
+export default function Journal() {
+  const { data, loading, error } = useQuery(journalPageQuery);
+  return (
+    <div className="flex flex-col md:flex-row gap-4 p-5">
+      <div>
+        <Label className="">Journal</Label>
+        <Textarea />
+        <Button>Save Note</Button>
+      </div>
+      <div>
+        <Input placeholder="Search for a note..." />
+        {data && <JournalsList journalsList={data} />}
+      </div>
+    </div>
+  );
 }
