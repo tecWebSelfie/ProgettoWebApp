ee551288eed57d0805cdc86dd32b6ad252612970
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-12-27 19:16:22 +0100

    added client typeDefs

--- src/app/Apollo.tsx
+++ src/app/Apollo.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
+  typeDefs: gql`
+    extend type Query {
+      TimeMachine: Date!
+    }
+
+    extend type Mutation {
+      TimeMachine: Date!
+    }
+  `,
 });
 
 export default function Apollo({
