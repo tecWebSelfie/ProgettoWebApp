5329c63701b78e485aadb6d214878790e8ee5ca6
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-07 13:45:54 +0100

    added layout with sidebar for all app sections

--- src/app/(sections)/layout.tsx
+++ src/app/(sections)/layout.tsx
@@ -1,12 +1,36 @@
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarContent,
+  SidebarInset,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenuItem,
+} from "@/src/components/ui/sidebar";
+
 export default function SectionLayout({
   children,
+  main,
+  sidebar,
 }: {
   children: React.ReactNode;
+  main: React.ReactNode;
+  sidebar: React.ReactNode;
 }) {
   return (
-    <>
-      <div>PROVA</div>
-      <div>{children}</div>
-    </>
+    <SidebarProvider>
+      <Sidebar
+        side="right"
+        collapsible="none"
+        variant="inset"
+        className="gap-4"
+      >
+        <SidebarInset>
+          <SidebarContent>{sidebar}</SidebarContent>
+        </SidebarInset>
+      </Sidebar>
+      <div className="w-full">{children ? children : main}</div>
+    </SidebarProvider>
   );
 }
