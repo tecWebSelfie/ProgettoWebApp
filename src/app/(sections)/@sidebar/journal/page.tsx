d53df0d842536aef8093dd6ad1c477076c3b1c99
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-07 13:47:47 +0100

    added parallel routes for parts of sections UI

---
+++ src/app/(sections)/@sidebar/journal/page.tsx
@@ -0,0 +1,16 @@
+import { JournalsList } from "@/src/components/JournalsList";
+import { Input } from "@/src/components/ui/input";
+import { JournalPageQueryDocument } from "@/src/gql/graphql";
+import { useQuery } from "@apollo/client";
+
+const journalPageQuery = JournalPageQueryDocument;
+
+export default function JournalSidebar() {
+  const { data } = useQuery(journalPageQuery);
+  return (
+    <>
+      <Input placeholder="Search for a journal..." />
+      {data && <JournalsList journalsList={data} />}
+    </>
+  );
+}
