642693190b06d3aa15b9f55e2e07e7051be5fd07
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-10 13:22:30 +0100

    added 'timeMachine' variable to apollo Client

--- src/app/Apollo.tsx
+++ src/app/Apollo.tsx
@@ -5,19 +5,35 @@ import {
   InMemoryCache,
   ApolloProvider,
   gql,
+  makeVar,
 } from "@apollo/client";
 import { finalLink } from "@/apolloLinks/finalLink";
+
+/*
+local variable implemented using Apollo's reactive variables.
+ to use it in a component, import it and call useReactiveVar(timeMachine)
+ to update it, call timeMachine(new Date())
+ to read it, call timeMachine()
+when updated, every component that uses it will re-render and active queries will re-fetch
+*/
+const timeMachine = makeVar(new Date());
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
   link: finalLink,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          TimeMachine: {
+            read: () => timeMachine(),
+          },
+        },
+      },
+    },
+  }),
   typeDefs: gql`
     extend type Query {
-      TimeMachine: Date!
-    }
-
-    extend type Mutation {
       TimeMachine: Date!
     }
   `,
