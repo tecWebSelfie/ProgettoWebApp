d14be5f68bb1c402e176f4c3ec994496cbfc6dc1
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-13 16:30:23 +0100

    apolloRSC now uses the schema directly

--- lib/apolloRSC.ts
+++ lib/apolloRSC.ts
@@ -1,13 +1,15 @@
-import { createFinalLink } from "@/src/apolloLinks/finalLink";
+import { SchemaLink } from "@apollo/client/link/schema";
 import {
   registerApolloClient,
   ApolloClient,
   InMemoryCache,
 } from "@apollo/experimental-nextjs-app-support";
+import { schema } from "@/db/gqlschema";
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: createFinalLink(),
+    //since we are on server we can use schemaLink (no need of an http request)
+    link: new SchemaLink({ schema, validate: true }),
   });
 });
