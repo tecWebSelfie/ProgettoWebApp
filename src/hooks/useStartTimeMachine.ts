a2e906a3d0cf51eb3f2512fd6aec2517de0cac96
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-13 11:28:52 +0100

    removed debug logs

--- src/hooks/useStartTimeMachine.ts
+++ src/hooks/useStartTimeMachine.ts
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { timeMachine } from "@/src/reactiveVars";
+import { time } from "console";
 
 export const useStartTimeMachine = () => {
+  const timerRef = useRef(false);
   useEffect(() => {
-    console.log("Starting time machine");
-    setInterval(() => {
-      timeMachine(new Date(timeMachine().getTime() + 1000));
-    }, 1000);
+    if (!timerRef.current) {
+      timerRef.current = true;
+      setInterval(() => {
+        timeMachine(new Date(timeMachine().getTime() + 1000));
+      }, 1000);
+    }
   }, []);
 };
