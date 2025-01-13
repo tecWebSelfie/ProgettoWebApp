3ab904e3846dfce64154d93f0668380d98a5df8c
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-13 18:44:21 +0100

    removed timeMachine from redux (redux will probably be taken out of project

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { counter } from "./slices/counterSlice";
-import { TM_Offset } from "./slices/TM_OffsetSlice";
 
 export const makeStore = () =>
   configureStore({
     reducer: {
       counter,
-      TM_Offset,
     },
   });
 
