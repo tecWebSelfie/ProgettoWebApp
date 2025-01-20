b784638a4b00898286899fcd191dda15a935cb8a
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-20 01:24:06 +0100

    added mobile layout. In md sidebar is now 1/3 and main is 2/3

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SidebarProvider, Sidebar, SidebarContent } from "./ui/sidebar";
 
 export const SectionLayout: React.FC<{
@@ -6,11 +6,16 @@ export const SectionLayout: React.FC<{
   sidebar: React.ReactNode;
 }> = ({ main, sidebar }) => {
   return (
-    <SidebarProvider className="gap-2">
-      <Sidebar collapsible="none">
-        <SidebarContent>{sidebar}</SidebarContent>
-      </Sidebar>
-      <main className="w-full flex flex-col gap-3">{main}</main>
-    </SidebarProvider>
+    <>
+      <SidebarProvider className="gap-2 hidden md:flex">
+        <Sidebar collapsible="none" className="md:basis-1/3">
+          <SidebarContent>{sidebar}</SidebarContent>
+        </Sidebar>
+        <main className="w-full flex flex-col gap-3">{main}</main>
+      </SidebarProvider>
+      <div className="flex flex-col gap-3 md:hidden">
+        {main} {sidebar}
+      </div>
+    </>
   );
 };
