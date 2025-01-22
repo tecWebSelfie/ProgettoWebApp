72d2c53b6df5f66a7c47727c8613ef991ad7ff09
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-01-22 13:36:38 +0100

    miscellaneus change

--- src/server/dotEnv.ts
+++ src/server/dotEnv.ts
@@ -2,13 +2,11 @@ import { config as configEnv } from "dotenv";
 
 console.log("node env is ", process.env.NODE_ENV);
 
-export const dev = process.env.NODE_ENV === "development";
-
 configEnv({
   path: [
     ".env",
     ".env.local",
-    ...(dev
+    ...(process.env.NODE_ENV === "development"
       ? [".env.development", ".env.development.local"]
       : [".env.production", ".env.production.local"]),
   ],
