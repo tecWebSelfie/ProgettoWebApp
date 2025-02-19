66f904ec6034c9c6684f7c0ea81a6ee17521dfb4
MicheleGennari <michele.gennari.lavoro@gmail.com>
2025-02-19 02:57:16 +0100

    added pino logging

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -12,7 +12,6 @@ mongoose.connect(dbConfig.uri).catch((error) => {
 });
 
 const routeHandler = auth(async (request) => {
-  console.log("I'm in route handler, user is: " + request.auth?.user.nickname);
   return await yoga.handle(request);
 });
 
--- src/app/graphql/yogaServer.ts
+++ src/app/graphql/yogaServer.ts
@@ -9,13 +9,12 @@ import { mockedResolvers, mocks } from "./mocks";
 import { myContext as context } from "./context";
 import type { YogaContext } from "./context";
 import { Resolvers } from "@/src/gql/resolvers-types";
+import { yogaAuthLogger } from "@/lib/pinoConfig";
 //this is the function that will be passed to genericAuth. It must return either the user object or null
 const resolveUserFn: ResolveUserFn<User, YogaContext> = async function (
   context,
 ) {
-  console.log(
-    "This output is in resolveUserFn(), " + context.request.auth?.user.scope,
-  );
+  yogaAuthLogger.info(context.request.auth?.user);
   return context.request.auth?.user;
 };
 
